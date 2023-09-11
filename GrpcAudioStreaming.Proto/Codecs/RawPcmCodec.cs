6e65e8e47de3c8dbba7504c5d4aa9cb662861ab8
random <fredericmorin@outlook.com>
2023-09-11 10:15:45 -0400

    fixed raw pcm codec

--- GrpcAudioStreaming.Proto/Codecs/RawPcmCodec.cs
+++ GrpcAudioStreaming.Proto/Codecs/RawPcmCodec.cs
@@ -1,15 +1,25 @@
-﻿namespace GrpcAudioStreaming.Proto.Codecs
+﻿using System;
+
+namespace GrpcAudioStreaming.Proto.Codecs
 {
     public class RawPcmCodec : ICodec
     {
         public byte[] Encode(byte[] data, int offset, int length)
         {
-            return data;
+            var encoded = new byte[length];
+
+            Array.Copy(data, offset, encoded, 0, length);
+
+            return encoded;
         }
 
         public byte[] Decode(byte[] data, int offset, int length)
         {
-            return data;
+            var decoded = new byte[length];
+
+            Array.Copy(data, offset, decoded, 0, length);
+
+            return decoded;
         }
     }
 }
